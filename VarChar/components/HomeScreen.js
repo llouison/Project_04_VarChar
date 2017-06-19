aa568b55a3f71606ed77bd23a796dc07d97d9526
lisa louison <lisa.louison@gmail.com>
2017-06-18 23:31:21 -0400

    added logo

--- VarChar/components/HomeScreen.js
+++ VarChar/components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, AppRegistry, Alert, Picker } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, Text, AppRegistry, Alert, Picker } from 'react-native';
 import { StackNavigator, NavigationActions } from 'react-navigation';
 
 export default class HomeScreen extends React.Component {
@@ -7,6 +7,7 @@ export default class HomeScreen extends React.Component {
     super(props);
     this.state = {
       player: 'Player 1',
+      logo: 'http://responsibledown.org/wp-content/uploads/2015/07/faq-icon.png',
     };
     this.showInstructions=this.showInstructions.bind(this);
   }
@@ -23,12 +24,14 @@ export default class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>VarChar</Text>
+        <Text style={styles.logo}>?</Text>
         <View>
+        {/*<Image source={{uri:this.state.logo}} style={{width: 250, height: 200, margin: 20}}/>
         <Picker selectedValue={this.state.player}
             onValueChange={(itemValue, itemIndex) => this.setState({player: itemValue})}>
             <Picker.Item label="Player 1" color='#E64230' value="Player 1" />
             <Picker.Item label="Player 2" color='#E64230' value="Player 2" />
-        </Picker>
+        </Picker>*/}
         <Text style={styles.invisible}>Choose your position</Text>
         </View>
         <TouchableOpacity onPress={() => this.showInstructions()}>
@@ -57,6 +60,15 @@ const styles = StyleSheet.create({
     fontFamily: 'MarkerFelt-Wide',
     fontSize: 60,
     fontWeight: 'bold',
+    color: '#FE5F55',
+    textShadowColor: '#77B5B7',
+    textShadowOffset: {width: 1, height: 1}
+  },
+  logo: {
+    fontFamily: 'AmericanTypewriter-Bold', 
+    fontSize: 160,
+    margin: 20,
+    alignSelf: 'center',
     color: '#FE5F55',
     textShadowColor: '#77B5B7',
     textShadowOffset: {width: 1, height: 1}
